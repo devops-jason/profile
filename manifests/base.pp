e80cb85a2b917b2395efd9207fe8aff61bd2fef9
Jason Lee Miller <jasonlmiller@jlmillerelectric.com>
2023-09-19 13:19:24 -0700

    Adding host resources for hostname, localhost, puppetserver_fqdn

--- manifests/base.pp
+++ manifests/base.pp
@@ -4,5 +4,23 @@
 #
 # @example
 #   include profile::base
-class profile::base {
+class profile::base (
+    String $puppetserver_fqdn = 'puppet.local'
+    String $puppetserver_ip   = '192.168.1.5',
+    String $host_file         = '/etc/hosts',
+) {
+
+    host { 'localhost':
+        ensure       => 'present',
+        host_aliases => ['localhost.localdomain', $facts['fqdn'], $facts['hostame']],
+        ip           => '127.0.0.1',
+        target       => $host_file,
+    }
+
+    host { $puppetserver_fqdn:
+        ensure      => 'present',
+        host_alises => ['puppet'],
+        ip          => $puppetserver_ip,
+        target      => $host_file,
+    }
 }
