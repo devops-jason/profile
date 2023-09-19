ffb7f3be5d578f794e6d2b4d2d49e780a9d9474f
Jason Lee Miller <jasonlmiller@jlmillerelectric.com>
2023-09-19 09:27:11 -0700

    Adding classes profile::puppet::puppet_server, profile::puppet::puppet_client, profile::base

---
+++ manifests/puppet/puppet_server.pp
@@ -0,0 +1,8 @@
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include profile::puppet::puppet_server
+class profile::puppet::puppet_server {
+}
