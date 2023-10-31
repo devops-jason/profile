dc920afb1101e42f3fb5f4d6bef0e4d4f52bd08c
Jason Lee Miller <jasonlmiller@jlmillerelectric.com>
2023-10-31 12:28:53 -0700

    Adding updated puppet_server profile for the updated puppet_server module

--- manifests/osx/base.pp
+++ manifests/osx/base.pp
@@ -6,18 +6,18 @@
 #   include profile::osx::base
 class profile::osx::base {
 
-    user { 'jasonmiller':
-        ensure     => 'present',
-        comment    => 'Jason Lee Miller',
-        gid        => 20,
-        groups     => ['_appserveradm', '_appserverusr', '_lpadmin', 'admin'],
-        home       => '/Users/jasonmiller',
-        iterations => 35460,
-        password   => 'e6162685501093e1c35098ef1ec7084d6b2010df3fcc40026f1116f0096174b61cf0217433ceb729b85f51437c06604e7c4353fbda28d17cdf89527b96a51ec227a74b7710d04222358654cade9014a652c6eb877d12ba9d5d13efe81c03dd4ca693a0a7062eb7b4ec6c13e9204d80d44f8a9b7747623affe28b310b09ba54b8',
-        provider   => 'directoryservice',
-        salt       => 'a7bcc191b36f0284b471334f26ccd4079aceff606d92bbd9ee35544554aedf5d',
-        shell      => '/bin/zsh',
-        uid        => 501,
-    }
+  user { 'jasonmiller':
+    ensure     => 'present',
+    comment    => 'Jason Lee Miller',
+    gid        => 20,
+    groups     => ['_appserveradm', '_appserverusr', '_lpadmin', 'admin'],
+    home       => '/Users/jasonmiller',
+    iterations => 35460,
+    password   => 'e6162685501093e1c35098ef1ec7084d6b2010df3fcc40026f1116f0096174b61cf0217433ceb729b85f51437c06604e7c4353fbda28d17cdf89527b96a51ec227a74b7710d04222358654cade9014a652c6eb877d12ba9d5d13efe81c03dd4ca693a0a7062eb7b4ec6c13e9204d80d44f8a9b7747623affe28b310b09ba54b8',
+    provider   => 'directoryservice',
+    salt       => 'a7bcc191b36f0284b471334f26ccd4079aceff606d92bbd9ee35544554aedf5d',
+    shell      => '/bin/zsh',
+    uid        => 501,
+  }
 
 }
--- manifests/puppet/puppet_server.pp
+++ manifests/puppet/puppet_server.pp
@@ -4,6 +4,22 @@
 #
 # @example
 #   include profile::puppet::puppet_server
-class profile::puppet::puppet_server {
-  include puppet_server
+#
+# @param r10k_control_repo
+#   The git url for the control-repo used by r10k on the puppetserver
+#
+# @param min_heap_size
+#   The minimum heap size used by the puppetserver specified in /etc/sysconfig/puppetserver
+#
+# @param max_heap_size
+#   The maximum heap size used by the puppetserer specified in /etc/sysconfig/puppetserver
+class profile::puppet::puppet_server (
+  String $r10k_control_repo,
+  String $min_heap_size = '512m',
+  String $max_heap_size = '512m'
+) {
+  class { 'puppet_server':
+    r10k_control_repo => $r10k_control_repo,
+    min_heap_size     => $min_heap_size,
+    max_heap_size     => $max_heap_size,
 }
