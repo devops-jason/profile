e9d161bbb0cdbeae4e6ce741ba0c9b6b19cded7a
Jason Lee Miller <jasonlmiller@jlmillerelectric.com>
2023-10-31 13:45:13 -0700

    Setting the parameters for puppet_server class in the manifest

--- manifests/puppet/puppet_server.pp
+++ manifests/puppet/puppet_server.pp
@@ -5,22 +5,10 @@
 # @example
 #   include profile::puppet::puppet_server
 #
-# @param r10k_control_repo
-#   The git url for the control-repo used by r10k on the puppetserver
-#
-# @param min_heap_size
-#   The minimum heap size used by the puppetserver specified in /etc/sysconfig/puppetserver
-#
-# @param max_heap_size
-#   The maximum heap size used by the puppetserer specified in /etc/sysconfig/puppetserver
-class profile::puppet::puppet_server (
-  String $r10k_control_repo,
-  String $min_heap_size = '512m',
-  String $max_heap_size = '512m'
-) {
+class profile::puppet::puppet_server {
   class { 'puppet_server':
-    r10k_control_repo => $r10k_control_repo,
-    min_heap_size     => $min_heap_size,
-    max_heap_size     => $max_heap_size,
+    min_heap_size     => '512m',
+    max_heap_size     => '512m',
+    r10k_control_repo => 'git@github.com:devops-jason/controlrepo.git',
   }
 }
